d9f3c1a6158788125f4d62cc1562115049a74778
SnehKalariya <snehkalariya9583@gmail.com>
2023-04-04 12:14:26 +0530

    complete

--- collectionView&RedioButton/ViewController.swift
+++ collectionView&RedioButton/ViewController.swift
@@ -2,18 +2,49 @@
 //  ViewController.swift
 //  collectionView&RedioButton
 //
-//  Created by Sneh kalariya on 03/04/23.
+//  Created by Sneh kalariya on 13/02/23.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    
+    var photo = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8"),UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8"),UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8"),UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8")]
+    var name = ["no1","no2","no3","no4","no5","no6","no7","no8","no1","no2","no3","no4","no5","no6","no7","no8","no1","no2","no3","no4","no5","no6","no7","no8","no1","no2","no3","no4","no5","no6","no7","no8"]
+    
+    var select = -1
+    
+//    var arr = Array<String>()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCollectionViewCell
+        cell.imageView.image = photo[indexPath.row]
+        cell.label.text = name[indexPath.row]
+       
+        
+        if select == indexPath.row{
+            cell.radioButton.image = UIImage(systemName: "circle.fill")
+        }
+        else{
+            cell.radioButton.image = UIImage(systemName: "circle")
+        }
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        select = indexPath.row
+        collectionView.reloadData()
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 272, height: 239)
+    }
 }
-
---
+++ collectionView&RedioButton/myCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  myCollectionViewCell.swift
+//  collectionView&RedioButton
+//
+//  Created by Sneh kalariya on 03/04/23.
+//
+
+import UIKit
+
+class myCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var radioButton: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
